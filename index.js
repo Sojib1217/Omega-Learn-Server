0844db2357703fb12f3d60525be30d9fc473779c
Sojib1217 <sojibhasan1888@gmail.com>
2025-11-10 18:36:26 +0600

    server side project setup done

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app=express();
+const cors = require('cors');
+const port=process.env.PORT || 3000;
+
+
+// middleWare
+app.use(cors());
+app.use(express.json())
+
+
+
+app.get('/',(req,res)=>{
+    res.send('omega learn running')
+})
+
+
+app.listen(port,()=>{
+    console.log(`This server listening on port ${port}`)
+})
